7d3207207774fb3b1376103deca484e6ec83cbed
nolayercode <vyelhekar@gmail.com>
2022-10-03 20:43:40 +0530

    updated the ipAddress parser program

--- ipAddress.py
+++ ipAddress.py
@@ -3,34 +3,13 @@
 # 	b) LIST OF ALL UNIQUE IP ADDRESSES
 # 	c) LIST OF IP ADDRESSES & THE VISITED URLS
 
-# def ipAddress(logfile_path):
-# 	ipList = {}
-# 	Contents = open(logfile_path, "r")
-# 	# You can use .readlines in old Python, but if the log is huge...
 
-# 	# Go through each line of the logfile
-# 	for line in Contents:
-# 		# Split the string to isolate the IP address
-# 		Ip = line.split(" ")[0]
-
-# 		if 6 < len(Ip) <= 15:
-# 			# Increase by 1 if IP exists; else set hit count = 1
-# 			ipList[Ip] = ipList.get(Ip, 0) + 1
-
-# 	print("All the IPs: ",ipList)
-
-# ipAddress('log_file\\apache_logs.txt')
 import re
 import csv
 
-# [(\d\.)]+) - - \[(.*?)\] "(.*?)" (\d+) (\d+) (.+)
-# def printAllIp():
-
-
 def ipAddress():
 	pattern = '([(\d\.)]+) - - \[(.*?)\] "(.*?)" (\d+) (.+)'
-	logfile_path = 'log_file\\apache_logs_2.txt'
-	urlPatterm = '^(http[s]?:\\/\\/(www\\.)?|ftp:\\/\\/(www\\.)?|www\\.){1}([0-9A-Za-z-\\.@:%_\+~#=]+)+((\\.[a-zA-Z]{2,3})+)(/(.)*)?(\\?(.)*)?'
+	logfile_path = 'log_file\\apache_logs.txt'
 	Contents = open(logfile_path, "r")
 	ipList = {}
 	urlList = {}
@@ -39,14 +18,32 @@ def ipAddress():
 		log_groups = re.match(pattern, line).groups()
 		ipList[log_groups[0]] = ipList.get(log_groups[0], 0) + 1
 		splittedURL = log_groups[4].split('"')
-		# urls = re.match(urlPatterm, splittedURL)
-		print(splittedURL)
-		# urlList[log_groups[0]] = log_groups[4]
-		# print(urls)
+		subUrl = splittedURL[3].split('+')
+		urls = ""
+		if splittedURL[1] != '-':
+			urls = splittedURL[1]
+		elif len(subUrl) >= 2:
+			urls = subUrl[1].strip("()")
+		else:
+			urls = "-"
+		urlList[log_groups[0]] = urls
 
-	# print(ipList)
-	# print(urlList)
-	# print(log_groups[4])
+	# print("All IP List:\n",ipList,"\n\n")
+	print("All IP and URL:\n",urlList,"\n\n")
+	
+	# field_names = ['IP', 'URL']
+	# with open('IpAddress.csv', 'w') as csvfile:
+	# 	writer = csv.DictWriter(csvfile, fieldnames=field_names)
+	# 	writer.writeheader()
+	# 	writer.writerows(urlList)
+
+	# try:
+	# 	geeky_file = open('ipAddress.txt', 'wt')
+	# 	geeky_file.write(str(urlList))
+	# 	geeky_file.close()
+
+	# except:
+	# 	print("Unable to write to file")
 
 def main():
 	ipAddress()
